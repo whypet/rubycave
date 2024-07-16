836298257f67d91409828bad0c3b64e4f90b70f9
whypet <47226783+whypet@users.noreply.github.com>
2024-07-16 13:28:26 +0200

    feat: create chunk concept

---
+++ rubycave/src/world.rs
@@ -0,0 +1,12 @@
+pub const CHUNK_WIDTH: usize = 16;
+pub const CHUNK_LENGTH: usize = 16;
+pub const CHUNK_HEIGHT: usize = 256;
+
+pub enum BlockId {
+    Air,
+    Grass,
+}
+
+pub struct Chunk {
+    blocks: [BlockId; CHUNK_WIDTH * CHUNK_LENGTH * CHUNK_HEIGHT],
+}
